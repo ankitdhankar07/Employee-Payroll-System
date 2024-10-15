96fe773c9304e4769183839100ed2ed89412570f
ankitdhankar07 <ankitdhankar16@gmail.com>
2024-10-16 00:12:27 +0530

    Created Employee Class

--- src/main/java/com/ankit/Main.java
+++ src/main/java/com/ankit/Main.java
@@ -1,4 +1,26 @@
 package com.ankit;
+
+
+abstract class Employee{
+    private String name;
+    private int id;
+    public Employee(String name , int id) {
+        this.id = id;
+        this.name=name;
+    }
+    public String getName (){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+    public abstract double CalculateEmployeeSalary();
+
+    @Override
+    public String toString(){
+        return "Employee [name = "+name+" , id= "+ id+", Salary = "+CalculateEmployeeSalary()+" ]";
+    }
+}
 public class Main {
     public static void main(String[] args) {
     }
